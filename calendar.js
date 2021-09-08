f72ffdbc605c8cbe1b43a5a3fefa48b529ab3bc3
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2021-09-08 21:59:08 +0900

    Modify: 불필요한 요소 제거 및 캘린더 자바스크립트 코드 수정

--- calendar.js
+++ calendar.js
@@ -1,8 +1,8 @@
 GitHubCalendar(".calendar", "Hschan2", { responsive: true, tooltips: false, global_stats: false, summary_text: false}).then(function() {
-    // delete the space underneath the module bar which is caused by minheight
+    // Github Calender Graph Style
     document.getElementsByClassName('calendar')[0].style.minHeight = "100px";
     document.getElementsByClassName('calendar')[0].style.maxWidth = "450px";
     document.getElementsByClassName('calendar')[0].style.border = "none";
-    // hide more and less legen below the contribution graph
+    // Graph 밑에 Less More 숨기기
     document.getElementsByClassName('contrib-legend')[0].style.display = "none";
 });
