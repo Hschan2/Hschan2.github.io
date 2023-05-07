814410623c19b38b6f6df8089855ab3939ffe72f
Hchan2 <seongchan_@naver.com>
2023-05-07 13:31:09 +0900

    기존 파일들, before 폴더로 이동

--- index.js
+++
@@ -1,8 +0,0 @@
-GitHubCalendar(".calendar", "Hschan2", { responsive: true, tooltips: false, global_stats: false, summary_text: false}).then(function() {
-    // delete the space underneath the module bar which is caused by minheight
-    document.getElementsByClassName('calendar')[0].style.minHeight = "100px";
-    document.getElementsByClassName('calendar')[0].style.maxWidth = "450px";
-    document.getElementsByClassName('calendar')[0].style.border = "none";
-    // hide more and less legen below the contribution graph
-    document.getElementsByClassName('contrib-legend')[0].style.display = "none";
-});
